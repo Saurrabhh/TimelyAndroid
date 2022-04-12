2021a21587c90b663f0269399288b167f6acb433
Saurrabhh <saurabhkyadav99@gmail.com>
2022-04-12 13:55:51 +0530

    editable added in mainusername

--- app/src/main/java/com/example/timely/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/timely/fragments/ProfileFragment.kt
@@ -1,23 +1,29 @@
 package com.example.timely.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.InputType
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.navigation.Navigation
 import com.example.timely.R
 import com.example.timely.databinding.FragmentProfileBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
 
     lateinit var binding: FragmentProfileBinding
     private lateinit var database: DatabaseReference
     private lateinit var auth: FirebaseAuth
+    private lateinit var motionLayout: MotionLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,12 +32,17 @@ class ProfileFragment : Fragment() {
         // Inflate the layout for this fragment
         auth = FirebaseAuth.getInstance()
 
+
         binding =  FragmentProfileBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        motionLayout = binding.motionlayout
+        val mainusername = binding.MainUserName
+        mainusername.inputType = InputType.TYPE_NULL
 
         KEY.fragmentName = KEY().PROFILE
         database = FirebaseDatabase.getInstance("https://timely-524da-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("Users")
@@ -43,7 +54,7 @@ class ProfileFragment : Fragment() {
                 if ( user.child("email").value.toString() == currentemail ){
 
                     binding.MainName.text = user.child("name").value.toString()
-                    binding.MainUserName.text= user.child("username").value.toString()
+                    binding.MainUserName.setText(user.child("username").value.toString())
                     binding.MainURN.text = user.child("urn").value.toString()
                     binding.MainClass.text = user.child("rollno").value.toString()
                     binding.MainSection.text = user.child("section").value.toString()
@@ -58,9 +69,52 @@ class ProfileFragment : Fragment() {
             Toast.makeText(activity, "Failed to fetch data. Check your connection", Toast.LENGTH_SHORT).show()
         }
 
-        val navController= Navigation.findNavController(view)
-//        binding.Home.setOnClickListener{
-//            navController.navigate(R.id.action_profileFragment_to_mainFragment)
-//        }
+
+        motionLayout.addTransitionListener(object: MotionLayout.TransitionListener{
+            override fun onTransitionStarted(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int
+            ) {
+                Log.d("Started","Started")
+            }
+
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {
+                Log.d("Change","Change")
+            }
+
+
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+
+                if (motionLayout != null) {
+                    if (currentId == 2131362137){
+                        mainusername.inputType = InputType.TYPE_CLASS_TEXT
+                        Toast.makeText(activity, currentId.toString(), Toast.LENGTH_SHORT).show()
+                    }else{
+                        mainusername.inputType = InputType.TYPE_NULL
+                    }
+                }
+            }
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) {
+                Log.d("Trigger","Trigger")
+            }
+
+        })
+
+
+
+
     }
 }
